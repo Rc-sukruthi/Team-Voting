ad9269a9e0cdc72d07ea596a8322be00f3491c6c
Rc-sukruthi <rcsukruthi12@gmail.com>
2023-01-06 16:12:55 +0530

    backgroud changed

--- config.js
+++ config.js
@@ -3,7 +3,17 @@ import firebase from 'firebase';
 // add SDK Firebase
 var firebaseConfig = {
 
+  apiKey: "AIzaSyArTvU66lmC1gav_pjs-C-buW5Dy7RPbAw",
+  authDomain: "project-67-b4fec.firebaseapp.com",
+  projectId: "project-67-b4fec",
+  storageBucket: "project-67-b4fec.appspot.com",
+  messagingSenderId: "778244544436",
+  appId: "1:778244544436:web:92de909f5a2a3f82c204e1"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
